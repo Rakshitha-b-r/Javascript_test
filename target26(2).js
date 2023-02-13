c4b94333bb195e4464d9c7a4854f2d2e48f39d3e
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-13 11:59:18 +0530

    Update and rename target26(1).js to target26(2).js

---
+++ target26(2).js
@@ -0,0 +1,122 @@
+looker.plugins.visualizations.add({
+  // Id and Label are legacy properties that no longer have any function besides documenting
+  // what the visualization used to have. The properties are now set via the manifest
+  // form within the admin/visualizations page of Looker
+  id: "looker_table",
+  label: "Table",
+  options: {
+    font_size: {
+      type: "number",
+      label: "Font Size (px)",
+      default: 11
+    }
+  },
+  // Set up the initial state of the visualization
+  create: function (element, config) {
+    console.log(config);
+    // Insert a <style> tag with some styles we'll use later.
+    element.innerHTML = `
+      <style>
+        .table {
+          font-size: ${config.font_size}px;
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        .table-header {
+          background-color: #eee;
+          border: 1px solid black;
+          border-collapse: collapse;
+          font-weight: normal;
+        }
+        .table-cell {
+          padding: 5px;
+          border-bottom: 1px solid #ccc;
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+      </style>
+    `;
+     // Create a container element to let us center the text.
+  this._container = element.appendChild(document.createElement("div"));
+
+},
+// Render in response to the data or settings changing
+updateAsync: function (data, element, config, queryResponse, details, done) {
+  console.log(config);
+  // Clear any errors from previous updates
+  this.clearErrors();
+
+  // Throw some errors and exit if the shape of the data isn't what this chart needs
+  if (queryResponse.fields.dimensions.length == 0) {
+    this.addError({ title: "No Dimensions", message: "This chart requires dimensions." });
+    return;
+  }
+
+  /* Code to generate table
+   * In keeping with the spirit of this little visualization plugin,
+   * it's done in a quick and dirty way: piece together HTML strings.
+   */
+  var generatedHTML = `
+    <style>
+      .table {
+        font-size: ${config.font_size}px;
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      .table-header {
+        background-color: #eee;
+        border: 1px solid black;
+        border-collapse: collapse;
+        font-weight: normal;
+      }
+  .table-cell {
+    padding: 5px;
+    border-bottom: 1px solid #ccc;
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+   .table-row {
+    border: 1px solid black;
+    border-collapse: collapse;
+  }
+</style>
+`;
+generatedHTML += "<table class='table'>";
+generatedHTML += "<tr class='table-header'>";
+generatedHTML += `<th class='table-header' rowspan='2' colspan='2' > </th>`;
+generatedHTML += `<th class='table-header' rowspan='1'>Applicable limit</th>`;
+generatedHTML += "</tr>";
+
+generatedHTML += "<tr class='table-header'>";
+generatedHTML += `<th class='table-header'> 010 </th>`;
+generatedHTML += "</tr>";
+
+const header=['Non institutions','Institutions','Institutions in %','Globally Systemic Important Institutions (G-SIIs)'];
+
+// Loop through the different types of column types looker exposes
+for (column_type of ["dimension_like", "measure_like", "table_calculations"])
+{
+
+    // Look through each field (i.e. row of data)
+    for (field of queryResponse.fields[column_type])
+    {
+        let i=0;
+        // First column is the label
+        generatedHTML += `<tr><th class='table-header'>${field.label_short}</th>`;
+        generatedHTML += `<th class='table-header'>${header[i]}</th>`;
+        // Next columns are the data
+        for(row of data)
+        {
+            generatedHTML += `<td class='transpose-table-cell'>${LookerCharts.Utils.htmlForCell(row[field.name])}</td>`
+        }
+        generatedHTML += '</tr>';
+        i++;
+    }
+}
+   generatedHTML += "</table>";
+
+  this._container.innerHTML = generatedHTML;
+
+done();
+}
+});
