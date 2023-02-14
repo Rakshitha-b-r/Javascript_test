0777915d244a05a50c0082c9647d753ee8376603
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-14 11:49:18 +0530

    Update and rename target_28(14)_jstest.js to target_28(15)_jstest.js

--- target_28(15)_jstest.js
+++ target_28(15)_jstest.js
@@ -12,9 +12,24 @@ looker.plugins.visualizations.add({
     }
   },
   // Set up the initial state of the visualization
-  create: function (element, config) {
+  create: function (data,element, config,queryResponse) {
     console.log(config);
     var generatedHTML =  generateTableHeader();
+    generatedHTML += "<tr >";
+    for (field of queryResponse.fields.dimensions.concat(queryResponse.fields.measures)) {
+      generatedHTML += `<th>${field.label_short}</th>`;
+    }
+    generatedHTML += "</tr>";
+
+    // Next rows are the data
+    for (row of data) {
+      generatedHTML += "<tr class='table-row'>";
+      for (field of queryResponse.fields.dimensions.concat(queryResponse.fields.measures)) {
+        generatedHTML += `<td>${LookerCharts.Utils.htmlForCell(row[field.name])}</td>`;
+      }
+      generatedHTML += "</tr>";
+    }
+
     this._container.innerHTML = generatedHTML;
   },
 
@@ -68,24 +83,6 @@ looker.plugins.visualizations.add({
       </style>
     `;
 
-    generatedHTML += "<table class='table'>";
-    generatedHTML += "<tr class='table-header'>";
-    for (field of queryResponse.fields.dimensions.concat(queryResponse.fields.measures)) {
-      generatedHTML += `<th class='table-header'>${field.label_short}</th>`;
-    }
-    generatedHTML += "</tr>";
-
-    // Next rows are the data
-    for (row of data) {
-      generatedHTML += "<tr class='table-row'>";
-      for (field of queryResponse.fields.dimensions.concat(queryResponse.fields.measures)) {
-        generatedHTML += `<td class='table-cell'>${LookerCharts.Utils.htmlForCell(row[field.name])}</td>`;
-      }
-      generatedHTML += "</tr>";
-    }
-    generatedHTML += "</table>";
-
-    this._container.innerHTML = generatedHTML;
     done();
   }
 });
