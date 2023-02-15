1deca2249b96c8086d498e943bbd466b6b4ae9e0
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-15 13:11:21 +0530

    Update and rename target26(4).js to target26(5).js

---
+++ target26(5).js
@@ -0,0 +1,127 @@
+looker.plugins.visualizations.add({
+  // Id and Label are legacy properties that no longer have any function besides documenting
+  // what the visualization used to have. The properties are now set via the manifest
+  // form within the admin/visualizations page of Looker
+  id: "looker_table",
+  label: "Table",
+  options: {
+    font_size: {
+      type: "number",
+      label: "Font Size (px)",
+      default: 11
+    }
+  },
+  // Set up the initial state of the visualization
+  create: function (element, config) {
+    console.log(config);
+    // Insert a <style> tag with some styles we'll use later.
+    element.innerHTML = `
+      <style>
+        .table {
+          font-size: ${config.font_size}px;
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+        .table-header {
+          background-color: #eee;
+          border: 1px solid black;
+          border-collapse: collapse;
+          font-weight: normal;
+        }
+        .table-cell {
+          padding: 5px;
+          border-bottom: 1px solid #ccc;
+          border: 1px solid black;
+          border-collapse: collapse;
+        }
+      </style>
+    `;
+    // Create a container element to let us center the text.
+    this._container = element.appendChild(document.createElement("div"));
+
+  },
+  // Render in response to the data or settings changing
+  updateAsync: function (data, element, config, queryResponse, details, done) {
+    console.log(config);
+    // Clear any errors from previous updates
+    this.clearErrors();
+
+    // Throw some errors and exit if the shape of the data isn't what this chart needs
+    if (queryResponse.fields.dimensions.length == 0) {
+      this.addError({ title: "No Dimensions", message: "This chart requires dimensions." });
+      return;
+    }
+
+    /* Code to generate table
+     * In keeping with the spirit of this little visualization plugin,
+     * it's done in a quick and dirty way: piece together HTML strings.
+     */
+    var generatedHTML = `
+    <style>
+      .table {
+        font-size: ${config.font_size}px;
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      .table-header {
+        background-color: #eee;
+        border: 1px solid black;
+        border-collapse: collapse;
+        font-weight: normal;
+        position: absolute;
+        width: 70px;
+        height: 15px;
+        font-family: 'Verdana';
+        font-size: 11px;
+        align-items: center;
+        text-align: left;
+        letter-spacing: -0.03em;
+      }
+      .table-cell {
+        padding: 5px;
+        border-bottom: 1px solid #ccc;
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+      .table-row {
+        border: 1px solid black;
+        border-collapse: collapse;
+      }
+</style>
+`;
+    generatedHTML += "<table class='table'>";
+    generatedHTML += "<tr class='table-header'>";
+    generatedHTML += `<th class='table-header' rowspan='2' colspan='2' > </th>`;
+    generatedHTML += `<th class='table-header' rowspan='1' colspan='9' style="text-align:center"><b>Applicable limit</b></th>`;
+    generatedHTML += "</tr>";
+
+    generatedHTML += "<tr class='table-header'>";
+    generatedHTML += `<th class='table-header' colspan='9' style="font-size: 11px;"> 010 </th>`;
+    generatedHTML += "</tr>";
+
+    const header = ['Non institutions', 'Institutions', 'Institutions in %', 'Globally Systemic Important Institutions (G-SIIs)'];
+
+    // Loop through the different types of column types looker exposes
+    let i = 0;
+    for (column_type of ["dimension_like", "measure_like", "table_calculations"]) {
+
+      // Look through each field (i.e. row of data)
+      for (field of queryResponse.fields[column_type]) {
+        // First column is the label
+        generatedHTML += `<tr><th class='table-header'>${field.label_short}</th>`;
+        generatedHTML += `<th class='table-header'>${header[i]}</th>`;
+        // Next columns are the data
+        for (row of data) {
+          generatedHTML += `<td class='table-cell'>${LookerCharts.Utils.htmlForCell(row[field.name])}</td>`
+        }
+        generatedHTML += '</tr>';
+        i++;
+      }
+    }
+    generatedHTML += "</table>";
+
+    this._container.innerHTML = generatedHTML;
+
+    done();
+  }
+});
