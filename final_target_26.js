ba8cadc99daaefe7bd8b6add0bea669c479d5f79
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-16 14:56:21 +0530

    Update and rename target26_final.js to final_target_26.js

--- final_target_26.js
+++ final_target_26.js
@@ -21,7 +21,7 @@ looker.plugins.visualizations.add({
           font-size: ${config.font_size}px;
           border: 1px solid black;
           border-collapse: collapse;
-          marging:auto;
+          margin:auto;
         }
         .table-header {
           background-color: #eee;
