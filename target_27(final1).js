72971c7c03898b0f48d74a81d0be31de6dae2524
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-16 17:18:47 +0530

    Update target_27(final1).js

--- target_27(final1).js
+++ target_27(final1).js
@@ -49,7 +49,7 @@ looker.plugins.visualizations.add({
           width: 90px;
         }
         .thead{
-          position: sticky;
+          position: static;
           top: 0; 
           z-index: 3;
         }
@@ -122,7 +122,7 @@ looker.plugins.visualizations.add({
           border-collapse: collapse;
         }
         .thead{
-          position: sticky;
+          position: static;
           top: 0; 
           z-index: 3;
         }
