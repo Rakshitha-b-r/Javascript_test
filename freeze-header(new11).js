23ee90a0c57aaae3170730738d86504300367ec2
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-17 13:30:10 +0530

    Update and rename freeze-header(new10).js to freeze-header(new11).js

--- freeze-header(new11).js
+++ freeze-header(new11).js
@@ -269,7 +269,7 @@ function generateTableHeader() {
             'Exposure value before application of exemptions and CRM','ELIGIBLE CREDIT RISK MITIGATION (CRM) TECHNIQUES','(-) Amounts exempted','Exposure value after application of exemptions and CRM',
             'Code','Group or individual','Transactions where there is an exposure to underlying assets','Total original exposure','',
             '(-) Substitution effect of eligible credit risk mitigation techniques','(-) Funded credit protection other than substitution effect',
-            '(-) Real estate','','Direct exposures','Indirect exposures','Additional exposures arising from transactions where there is an exposure to underlying assets',
+            '(-) Real estate','abcdef','','Direct exposures','Indirect exposures','Additional exposures arising from transactions where there is an exposure to underlying assets',
             'Of which: defaulted','Debt instruments','Equity instruments','Derivatives','Off balance sheet items','Debt instruments','Equity instruments',
             'Derivatives','Off balance sheet items','Total','Of which: Non-trading book','% of Tier 1 capital','(-) Debt instruments','(-) Equity instruments',
             '(-) Derivatives','(-) Off balance sheet items','Total','Of which: Non-trading book','% of Tier 1 capital','Loan commit-ments','Financial guarantees',
