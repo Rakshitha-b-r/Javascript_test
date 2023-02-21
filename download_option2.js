3325a14d0354d934d0c1834ebc47a15471ef28fc
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-21 10:07:12 +0530

    Update and rename download_option1.js to download_option2.js

--- download_option2.js
+++ download_option2.js
@@ -118,6 +118,7 @@ looker.plugins.visualizations.add({
     var wb = XLSX.utils.table_to_book(document.querySelector("table"), {sheet:"Sheet1"});
     var filename = "data.xlsx";
     XLSX.writeFile(wb, filename);
+    console.log(window.URL.createObjectURL(new Blob([wb], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' })));
   });
 },
 
