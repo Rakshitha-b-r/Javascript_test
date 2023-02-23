f25605764d1a47e8c4267ff2b91d5e08254e664a
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-23 14:45:55 +0530

    Update and rename download-option3.js to download-option4.js

--- download-option4.js
+++ download-option4.js
@@ -72,7 +72,7 @@ looker.plugins.visualizations.add({
             , template = '<html xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:x="urn:schemas-microsoft-com:office:excel" xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{Worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="text/plain; charset=UTF-8"/></head><body><table>{table}</table></body></html>'
             , base64 = function(s) { return window.btoa(unescape(encodeURIComponent(s))) }
             , format = function(s, c) { return s.replace(/{(\w+)}/g, function(m, p) { return c[p]; }) }
-        var table = element.querySelector('table');
+        var table = document.querySelector('table');
         var ctx = {Worksheet: 'Worksheet', table: table.innerHTML}
         window.location.href = uri + base64(format(template, ctx))
       });
