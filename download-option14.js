10722c2e982626425ed5822f02bd9441f2e7b00c
Rakshitha-b-r <rakshithabr2@gmail.com>
2023-02-23 16:27:05 +0530

    Update and rename download-option13.js to download-option14.js

--- download-option14.js
+++ download-option14.js
@@ -92,11 +92,11 @@ looker.plugins.visualizations.add({
             var fontWeight = window.getComputedStyle(cell).fontWeight;
             var fontFamily = window.getComputedStyle(cell).fontFamily;
             var fontSize = window.getComputedStyle(cell).fontSize;
+            cell.setAttribute('style', 'background-color:' + backgroundColor);
             cell.setAttribute('style', 'border:' + border);
             cell.setAttribute('style', 'font-weight:' + fontWeight);
             cell.setAttribute('style', 'font-family:' + fontFamily);
             cell.setAttribute('style', 'font-family:' + fontSize);
-            cell.setAttribute('style', 'background-color:' + backgroundColor);
           }
         }
         var ctx = {Worksheet: 'Worksheet', table: table.innerHTML}
